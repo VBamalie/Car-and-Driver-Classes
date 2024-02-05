c516e14295978e2c94ef416aec4e3d14e2ee1561
Molly Von Behren <101467130+VBamalie@users.noreply.github.com>
2024-02-05 17:58:02 -0500

    Update CarAndDriver.java

--- CarAndDriver.java
+++ CarAndDriver.java
@@ -13,10 +13,9 @@ public class CarAndDriver {
         System.out.println("Can you drive a stick? Type 'Y' if you can.");
         driver.setStickDriver(input.nextLine().equalsIgnoreCase("Y")?true:false);
         System.out.println("How old are you?");
-        driver.setAge(input.nextInt());
+        driver.setAge(Integer.parseInt(input.nextLine()));
         System.out.println("How tall are you in inches?");
-        driver.setHeight(input.nextInt());
-        input.close();
+        driver.setHeight(Integer.parseInt(input.nextLine()));
         return driver;
     }//end userPrompt
 
@@ -35,6 +34,9 @@ public class CarAndDriver {
                 if(!element.getIsManual())
                     viableCars.add(element);
             }//endfor
+            if(viableCars.isEmpty()){
+                System.out.println("All the cars left are manual!");
+            }//endif
             return viableCars;
     }//end filterManuals
 
@@ -44,37 +46,55 @@ public class CarAndDriver {
             if(element.getHeadroom() > driver.getHeight()/2){
                 viableCars.add(element);
             }//endif
+            else{
+                System.out.println("Your too tall!");
+            }//endelse
          }//endfor
          return viableCars;
     }//end checkHeights
-    
-    public static void main(String[] args) throws Exception {
-        //TODO Print assignment info
-        System.out.printf("Java Advanced\nCOP-2805C-86399\nFebruary 1st, 2024\nMolly Von Behren\n\n");
 
+    static List<Car> connectUsersToCars(List<Car> availableCars){
         Person newDriver = userPrompt();//User creates a profile for themselves
-
-        //FUTURE DEVELOPMENT create more prompts that could narrow down a Person's preference
-    
-        List<Car> availableCars = populateCars();//creates a list of available cars for the User.
-
-        //FUTURE DEVELOPMENT potentially connect to a car database API that will can automatically populate a list of available cars instead of manually entering some cars?
-        
         if(!newDriver.getStickDriver()) //if the User cannot use Manual cars, this filters them out
             availableCars = filterManuals(availableCars);
         availableCars = checkHeights(newDriver, availableCars);//checks if the User is too tall for any cars
 
         int index = (int)(Math.random() * availableCars.size());// picks a random index off the list of available cars
         try {
+            availableCars.get(index).setDriver(newDriver);//sets the new driver for this car
             System.out.println("");
-            System.out.println(newDriver);
-            System.out.println(availableCars.get(index));//prints the random index that was previously made
+            System.out.println(availableCars.get(index).getDriver()); //prints out the info of the car's new driver
+            System.out.println(availableCars.get(index));//prints out the information of the car
+            System.out.println("");
+            availableCars.remove(index); //removes the car from the list of available cars
+            return availableCars;
         }catch(Exception e){
-            System.out.println("There are no cars available");// if there are no cars in the list, throws this exception
+            System.out.println("There are no cars available for you, Sorry");
+            return availableCars;// if there are no cars in the list, throws this exception
         }//endtry
+    }//end connectUsersToDrivers
+    
+    public static void main(String[] args) throws Exception {
+        System.out.printf("Java Advanced\nCOP-2805C-86399\nFebruary 1st, 2024\nMolly Von Behren\n\n");
+
+        //FUTURE DEVELOPMENT create more prompts that could narrow down a Person's preference
+    
+        List<Car> availableCars = populateCars();//creates a list of available cars for the User.
+        
+        availableCars = connectUsersToCars(availableCars);
+        availableCars = connectUsersToCars(availableCars);
+        availableCars = connectUsersToCars(availableCars);
+        if(!availableCars.isEmpty()){
+            System.out.println("Here are the cars that are still avaiable for purchase: ");
+            for (Car car : availableCars) {
+                System.out.println(car);
+            }//endfor
+        }//endif
+
+        //FUTURE DEVELOPMENT potentially connect to a car database API that will can automatically populate a list of available cars instead of manually entering some cars?
 
         //FUTURE DEVELOPMENT maybe allow the user to choose from a list of cars?
-        //FUTER DEVELOPMENT Create a proper app environment
-        
+        //FUTERE DEVELOPMENT Create a proper app environment
     }
 }
+
